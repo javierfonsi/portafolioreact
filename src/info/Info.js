25adf55869bb1a968693369654255e521dc7ddfb
javierfonsi <javier_fonsi@hotmail.com>
2022-08-10 15:04:54 -0500

    Some links

--- src/info/Info.js
+++ src/info/Info.js
@@ -120,32 +120,32 @@ export const info = {
     portfolio: [ // This is where your portfolio projects will be detailed
         {
             title: "Project 1",
-            live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-            source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
+            live: "https://portafolio-javierfonseca.netlify.app/", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+            source: "https://pokedex-javierfonseca.netlify.app", // this should be a link to the **repository** of the project, where the code is hosted.
             image: mock1
         },
         {
             title: "Project 2",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
+            live: "https://portafolio-javierfonseca.netlify.app/",
+            source: "https://github.com/javierfonsi",
             image: mock2
         },
         {
             title: "Project 3",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
+            live: "https://portafolio-javierfonseca.netlify.app/",
+            source: "https://github.com/javierfonsi",
             image: mock3
         },
         {
             title: "Project 4",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
+            live: "https://portafolio-javierfonseca.netlify.app/",
+            source: "https://github.com/javierfonsi",
             image: mock4
         },
         {
             title: "Project 5",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
+            live: "https://portafolio-javierfonseca.netlify.app/",
+            source: "https://github.com/javierfonsi",
             image: mock5
         }
     ]
